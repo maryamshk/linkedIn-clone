100578c7d95493768c6389c6a8b869981c8f3380
Maryam <maryamjaved.xo@gmail.com>
2023-08-23 15:23:35 +0500

    login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./components/Login";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" Component={Login}>
+
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
